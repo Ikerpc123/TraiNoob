2e05604b7613ab5178a33523fb26bbfdbd99787e
Iker <ikerpc73@educastur.es>
2025-06-04 21:53:35 +0200

    Actualizado

--- src/main/java/com/ikerpc123/trainoob/TrainoobApplication.java
+++ src/main/java/com/ikerpc123/trainoob/TrainoobApplication.java
@@ -1,24 +1,11 @@
 package com.ikerpc123.trainoob;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
 public class TrainoobApplication {
-	
-	@Bean
-	public Principal applicationStartupRunner() {
-		
-		return new Principal();
-		
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainoobApplication.class, args);
